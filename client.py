e25f28a59b9a7432fd653f0dc070fb013026c088
Daniel Davidov <dannydav34510@gmail.com>
2023-03-23 17:59:22 -0400

    added dynamic ip

--- client.py
+++ client.py
@@ -1,7 +1,21 @@
+import socket
 from socket import *
 
+def get_local_ip_address():
+    try:
+        # Create a temporary socket and connect to a dummy IP address
+        with socket(AF_INET, SOCK_DGRAM) as temp_socket:
+            temp_socket.connect(("10.255.255.255", 1))
+            local_ip_address = temp_socket.getsockname()[0]
+    except Exception:
+        # Fallback to loopback address if the method above fails
+        local_ip_address = "127.0.0.1"
+    return local_ip_address
+
+
 # TCP Client code from textbook
-serverName = '172.20.10.13'
+
+serverName = get_local_ip_address()
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
