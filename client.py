643d906814f10feb35059bd8a4d9db176d8db17e
Daniel Davidov <dannydav34510@gmail.com>
2023-03-27 01:31:23 -0400

    Ready for demo - There is one small bug to fix however, but its more like a feature. Need to resolve conflict between when a user is being prompted for input, and when the user is receiving a message.

--- client.py
+++ client.py
@@ -4,7 +4,8 @@ import mainModule
 import sys
 from threading import Thread
 
-
+###### If client receives a message while it is being prompted in the console, must save the prompt, output message, and reprompt #######
+previous_message = ""
 
 def main():
     #   Configure the client PORT and IP variables
@@ -19,37 +20,62 @@ def main():
 
     #   Make a thread for incoming connections from the server
     #   when the server is sending a message from another client
-    # client_receiver_thread = Thread(target=receive_messages_from_server, args=(clientIP, clientPort))
-    # client_receiver_thread.daemon = True
-    # client_receiver_thread.start()
+    client_receiver_thread = Thread(target=thread_receives_from_server, args=(clientIP, clientPort))
+    client_receiver_thread.daemon = True
+    client_receiver_thread.start()
 
+    register_with_server(serverIP, serverPort, userName, clientPort)
 
-    #   Sending messages
+    #   Sending messages for server to forward
     while True:
         #   Before making new connection, ask user if they want to send money?
-        if not input("Do you want to send crypto: ").lower() == "yes":
+        previous_message = "Do you want to send message: "
+        if not input("Do you want to send message: ").lower() == "yes":
             return
-        clients_connection_Socket = socket(AF_INET, SOCK_STREAM)
+        client_to_server_socket = socket(AF_INET, SOCK_STREAM)
+        previous_message = "Waiting for connection to server..."
         print("Waiting for connection to server...")
-        clients_connection_Socket.connect((serverIP, serverPort))
-        send_credentials(clients_connection_Socket, userName, clientPort)
-        message_exchange(clients_connection_Socket)
-        # clients_connection_Socket.shutdown(SHUT_RDWR)
-        clients_connection_Socket.close()
+        client_to_server_socket.connect((serverIP, serverPort))
+        send_credentials(client_to_server_socket, userName, clientPort)
+        # send_target_credentials(client_to_server_socket)
+        message_exchange(client_to_server_socket, userName)
+        # client_to_server_socket.shutdown(SHUT_RDWR)
+        client_to_server_socket.close()
 
 
-def receive_messages_from_server(clientIP, clientPort):
-    pass
-#     #  Creating the server's socket object
-#     client_listener_Socket = socket(AF_INET, SOCK_STREAM)
-#     # serverIP = get_local_ip_address()
-#     client_listener_Socket.bind((clientIP, clientPort))
-#     client_listener_Socket.listen(20)
-#     print('The server is ready to receive')
+def thread_receives_from_server(clientIP, clientPort):
+    #  Creating the client's socket object
+    client_listening_Socket = socket(AF_INET, SOCK_STREAM)
+    # serverIP = get_local_ip_address()
+    client_listening_Socket.bind((clientIP, clientPort))
+    client_listening_Socket.listen(20)
+    # print("The server is ready to receive")
+
+    while True:
+        #   Create the socket with which we will listen for messages from server, that are being forwarded
+        server_to_client_Socket, addr = client_listening_Socket.accept()
+        #   First receive the username of the origin client
+        origin_client_username = server_to_client_Socket.recv(16384).decode()
+        #   Send confirmation that received the username
+        server_to_client_Socket.send("Client Received username".encode())
+        #   Receive the Actual message
+        message = server_to_client_Socket.recv(16384).decode()
+        #   Send confirmation that received the message
+        server_to_client_Socket.send("Client Received message".encode())
+        if not message == "":
+            print("\n\nMessage received from " + origin_client_username + ": " + message)
+            print(previous_message)
+
+        try:
+            server_to_client_Socket.shutdown(SHUT_RDWR)
+        except error as e:
+            # print(f"Error shutting down socket: {e}")
+            pass
+        server_to_client_Socket.close()
 
 
 def send_credentials(clients_connection_Socket, userName, clientPort):
-    #   Send hardcoded Port number
+    #   Send real Port number (as opposed to what addr[1] holds)
     clients_connection_Socket.send(str(clientPort).encode())
     #   Getting confirmation that server got Port Number
     clients_connection_Socket.recv(16384).decode()
@@ -57,21 +83,52 @@ def send_credentials(clients_connection_Socket, userName, clientPort):
     clients_connection_Socket.send(userName.encode())
     #   Getting confirmation that server got Username
     clients_connection_Socket.recv(16384).decode()
+    # print("Connected, and credentials sent...")
+
+
+def message_exchange(socket_object, userName):
+    #   Send the target username
+    previous_message = 'To whom do you wish to send: '
+    target_userName = input('To whom do you wish to send: ')
+    socket_object.send(target_userName.encode())
+
+    #   Get confirmation that server received target username
+    target_confirmation = socket_object.recv(16384).decode()
+    if target_confirmation == "This user doesn't exist!":
+        previous_message = target_confirmation
+        print(target_confirmation)
+        return
+
+    #   Send origin username
+    socket_object.send(userName.encode())
+    #   Get confirmation that server received origin username
+    socket_object.recv(16384).decode()
+
+    #   Input message to send to target, and send to server
+    previous_message = 'Input message to send to ' + target_userName + " : "
+    message = input('Input message to send to ' + target_userName + " : ")
+    if message == "quit":
+        socket_object.shutdown(SHUT_RDWR)  # will send empty string to the server
+        socket_object.close()
+        return
+    socket_object.send(message.encode())
+
+    #   Get confirmation that the message was received by the server, which will attempt to forward it
+    socket_object.recv(16384).decode()
+
+    # modifiedSentence = socket_object.recv(16384)
+    # print('From Server: ', modifiedSentence.decode())
 
 
 
-    print("Connected, and credentials sent...")
+def register_with_server(serverIP, serverPort, userName, clientPort):
+    client_to_server_socket = socket(AF_INET, SOCK_STREAM)
+    print("Registering with server...")
+    client_to_server_socket.connect((serverIP, serverPort))
+    send_credentials(client_to_server_socket, userName, clientPort)
+    client_to_server_socket.send("".encode())
+    client_to_server_socket.close()
 
-
-def message_exchange(clients_connection_Socket):
-    sentence = input('Input lowercase sentence (type quit to exit): ')
-    if sentence == "quit":
-        clients_connection_Socket.shutdown(SHUT_RDWR)
-        clients_connection_Socket.close()
-        sys.exit()
-    clients_connection_Socket.send(sentence.encode())
-    modifiedSentence = clients_connection_Socket.recv(16384)
-    print('From Server: ', modifiedSentence.decode())
 
 
 
--- server.py
+++ server.py
@@ -4,7 +4,10 @@ from threading import Thread
 import threading
 import queue
 import mainModule
+
+
 resource_lock = threading.Lock()
+clientList_resource_Lock = threading.Lock()
 
 
 def main():
@@ -47,7 +50,7 @@ def main():
 
 def handle_client(connectionSocket, addr, clientList):
     check_credentials(connectionSocket, addr, clientList)
-    message_exchange(connectionSocket, addr, clientList)
+    message_forwarding(connectionSocket, addr, clientList)
     try:
         connectionSocket.shutdown(SHUT_RDWR)
     except error as e:
@@ -79,18 +82,50 @@ def check_credentials(connectionSocket, addr, clientList):
             print(curr_clients_username + " added to client list")
 
 
-def message_exchange(connectionSocket, addr, clientList):
-    sentence = connectionSocket.recv(16384).decode()
-    if not sentence == "":
-        #   Capitalize the sentence
-        capitalizedSentence = sentence.upper()
-        #   Send the capitalized sentence back to the client
-        connectionSocket.send(capitalizedSentence.encode())
+def message_forwarding(connectionSocket, addr, clientList):
+    #   Get target username from the origin client
+    target_username = connectionSocket.recv(16384).decode()
+    #   Check that the target exists, if not, will need to send message telling the client that they don't
+    target_client = find_in_clientList(clientList, target_username)
+    if target_client is None:
+        try:
+            connectionSocket.send("This user doesn't exist!".encode())
+        except ConnectionError:
+            pass
+        return
+
+    #   Send confirmation that received the target username
+    connectionSocket.send("Received target username...".encode())
+
+    #   Get origin userName
+    origin_userName = connectionSocket.recv(16384).decode()
+    #   Send confirmation that received the origin username
+    connectionSocket.send("Received origin username...".encode())
+
+    #   Get the message from the client to send to the target
+    message_to_forward = connectionSocket.recv(16384).decode()
+    #   Send confirmation that received the message
+    connectionSocket.send("Received Message to send to target client".encode())
+
+    if not message_to_forward == "":
+        #   Create the socket that will be used to send the message to the target
+        server_to_forward_Socket = socket(AF_INET, SOCK_STREAM)
+        server_to_forward_Socket.connect((target_client.clientIP, int(target_client.clientPort)))
+        #   Send the username of client that is sending the message:
+        server_to_forward_Socket.send(origin_userName.encode())
+        #   Receive confirmation that the client got the username
+        server_to_forward_Socket.recv(16384).decode()
+        #   Send the Actual message
+        server_to_forward_Socket.send(message_to_forward.encode())
+        #   Receive confirmation that client recieved message
+        server_to_forward_Socket.recv(16384).decode()
+
         #   Print an indication that all went well
-        print("Sent uppercase version of \"" + sentence + "\" to client")
+        print("Successfully forwarded message to: " + target_username)
     else:
-        connectionSocket.send("Cannot Capitalize empty string".encode())
-        print("Sent warning upon receiving empty string")
+        # connectionSocket.send("Cannot forward empty message".encode())
+        # print("Sent warning upon receiving empty string")
+        return
 
 
 #   Must use resource lock pre-using this function
@@ -103,6 +138,34 @@ def client_exists_in_client_list(clientList, client) -> bool:
     return False
 
 
+def check_username_exists_in_client_list(clientList, username) -> bool:
+
+    for i in clientList:
+        if i.userName == username:
+            return True
+    else:
+        return False
+
+
+def find_in_clientList(clientList, target_username):
+    with clientList_resource_Lock:
+        for i in clientList:
+            if i.userName == target_username:
+                return i
+        else:
+            return None
+
+
+def get_local_ip_address():
+    try:
+        # Create a temporary socket and connect to a dummy IP address
+        with socket(AF_INET, SOCK_DGRAM) as temp_socket:
+            temp_socket.connect(("10.255.255.255", 1))
+            local_ip_address = temp_socket.getsockname()[0]
+    except Exception:
+        # Fallback to loopback address if the method above fails
+        local_ip_address = "127.0.0.1"
+    return local_ip_address
 
 
 
